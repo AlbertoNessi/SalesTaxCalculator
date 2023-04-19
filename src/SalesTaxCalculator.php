bc5f43ca99831483d1bb2aa3a915a6ff5a94ac6a
Alberto <alberto.nessi@secgroup.it>
2023-04-19 23:48:28 +0200

    add sales tax and tot from controller

--- src/SalesTaxCalculator.php
+++ src/SalesTaxCalculator.php
@@ -1,10 +1,10 @@
 <?php
 
-$BasicSalesTax = "10";
+define("BASIC_SALES_TAX", 10);
 // all imported products, no exemptions
-$ImportDuty = "5";
+define("IMPORT_DUTY", 5);
 
-// those are the product types that are exempt from the BasicSalesTax
+// those are the product types that are exempt from the basicSalesTax
 $exceptTypes = [
     'books',
     'food',
@@ -12,82 +12,117 @@ $exceptTypes = [
 ];
 
 // inputs
-$Basket1 = [
-    [
-        'product_name' => "book",
-        'qty' => 2,
-        'type' => "books",
-        'imported' => false,
-        'price' => 12.49
+$baskets = [
+    "basket1" => [
+        [
+            'product_name' => "book",
+            'qty' => 2,
+            'type' => "books",
+            'imported' => false,
+            'price' => 12.49,
+        ],
+        [
+            'product_name' => "music CD",
+            'qty' => 1,
+            'type' => "music",
+            'imported' => false,
+            'price' => 14.99
+        ],
+        [
+            'product_name' => "chocolate bar",
+            'qty' => 1,
+            'type' => "food",
+            'imported' => false,
+            'price' => 0.85
+        ]
     ],
-    [
-        'product_name' => "music CD",
-        'qty' => 1,
-        'type' => "music",
-        'imported' => false,
-        'price' => 14.99
+    "basket2" => [
+        [
+            'product_name' => "box of chocolates",
+            'qty' => 2,
+            'type' => "food",
+            'imported' => true,
+            'price' => 10.00
+        ],
+        [
+            'product_name' => "bottle of perfume",
+            'qty' => 1,
+            'type' => "perfume",
+            'imported' => true,
+            'price' => 47.50
+        ]
     ],
-    [
-        'product_name' => "chocolate bar",
-        'qty' => 1,
-        'type' => "food",
-        'imported' => false,
-        'price' => 0.85
+    "basket3" => [
+        [
+            'product_name' => "bottle of perfume",
+            'qty' => 1,
+            'type' => "perfume",
+            'imported' => true,
+            'price' => 27.99
+        ],
+        [
+            'product_name' => "bottle of perfume",
+            'qty' => 1,
+            'type' => "perfume",
+            'imported' => false,
+            'price' => 18.99
+        ],
+        [
+            'product_name' => "packet of headache pills",
+            'qty' => 1,
+            'type' => "medical",
+            'imported' => false,
+            'price' => 9.75
+        ],
+        [
+            'product_name' => "box of imported chocolates",
+            'qty' => 3,
+            'type' => "food",
+            'imported' => true,
+            'price' => 11.25
+        ]
     ]
 ];
 
-$Basket2 = [
-    [
-        'product_name' => "box of chocolates",
-        'qty' => 2,
-        'type' => "food",
-        'imported' => true,
-        'price' => 10.00
-    ],
-    [
-        'product_name' => "bottle of perfume",
-        'qty' => 1,
-        'type' => "perfume",
-        'imported' => true,
-        'price' => 47.50
-    ]
-];
+foreach ($baskets as &$basket) {
+    $taxes = 0;
+    $tot_cost = 0;
 
-$Basket3 = [
-    [
-        'product_name' => "bottle of perfume",
-        'qty' => 1,
-        'type' => "perfume",
-        'imported' => true,
-        'price' => 27.99
-    ],
-    [
-        'product_name' => "bottle of perfume",
-        'qty' => 1,
-        'type' => "perfume",
-        'imported' => false,
-        'price' => 18.99
-    ],
-    [
-        'product_name' => "packet of headache pills",
-        'qty' => 1,
-        'type' => "medical",
-        'imported' => false,
-        'price' => 9.75
-    ],
-    [
-        'product_name' => "box of imported chocolates",
-        'qty' => 3,
-        'type' => "food",
-        'imported' => true,
-        'price' => 11.25
-    ]
-];
+    foreach ($basket as $kBasket => $vBasket) {
+        foreach ($vBasket as $key => $value) {
+
+            if ($key == "type" && in_array($value, $exceptTypes)) {
+                $except = true;
+            } else {
+                $except = false;
+            }
+            
+            if ($key == "price"){
+                if(!$except) {
+                    $taxes += $value * BASIC_SALES_TAX / 100;
+                }
+                $tot_cost += $value;
+            }
+
+            if ($key == "imported") {
+                if ($value) {
+                    $taxes += $value * IMPORT_DUTY / 100;;
+                }
+            }
+
+            if ($key == "qty") {
+                $taxes *= $value;
+                $tot_cost *= $value;
+            }
+        }
+    }
+    $basket['sales_taxes'] = $taxes;
+    $basket['total'] = $tot_cost;
+}
 
 $return = [
-    "basket1" => $Basket1, 
-    "basket2" => $Basket2, 
-    "basket3" => $Basket3
+    "baskets" => $baskets,
+    'exceptTypes' => $exceptTypes
 ];
 
 echo json_encode($return);
