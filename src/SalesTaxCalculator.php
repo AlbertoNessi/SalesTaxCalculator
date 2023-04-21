e79bb671728df3bcda5d0a126523e7d480a4a9af
Alberto <alberto.nessi@secgroup.it>
2023-04-21 07:23:05 +0200

    add price_qty and taxes for each product

--- src/SalesTaxCalculator.php
+++ src/SalesTaxCalculator.php
@@ -20,20 +20,26 @@ $baskets = [
             'type' => "books",
             'imported' => false,
             'price' => 12.49,
+            'price_qty' => 0,
+            'taxes' => 0
         ],
         [
             'product_name' => "music CD",
             'qty' => 1,
             'type' => "music",
             'imported' => false,
-            'price' => 14.99
+            'price' => 14.99,
+            'price_qty' => 0,
+            'taxes' => 0
         ],
         [
             'product_name' => "chocolate bar",
             'qty' => 1,
             'type' => "food",
             'imported' => false,
-            'price' => 0.85
+            'price' => 0.85,
+            'price_qty' => 0,
+            'taxes' => 0
         ]
     ],
     "basket2" => [
@@ -42,14 +48,18 @@ $baskets = [
             'qty' => 2,
             'type' => "food",
             'imported' => true,
-            'price' => 10.00
+            'price' => 10,
+            'price_qty' => 0,
+            'taxes' => 0
         ],
         [
             'product_name' => "bottle of perfume",
             'qty' => 1,
             'type' => "perfume",
             'imported' => true,
-            'price' => 47.50
+            'price' => 47.50,
+            'price_qty' => 0,
+            'taxes' => 0
         ]
     ],
     "basket3" => [
@@ -58,66 +68,95 @@ $baskets = [
             'qty' => 1,
             'type' => "perfume",
             'imported' => true,
-            'price' => 27.99
+            'price' => 27.99,
+            'price_qty' => 0,
+            'taxes' => 0
         ],
         [
             'product_name' => "bottle of perfume",
             'qty' => 1,
             'type' => "perfume",
             'imported' => false,
-            'price' => 18.99
+            'price' => 18.99,
+            'price_qty' => 0,
+            'taxes' => 0
         ],
         [
             'product_name' => "packet of headache pills",
             'qty' => 1,
             'type' => "medical",
             'imported' => false,
-            'price' => 9.75
+            'price' => 9.75,
+            'price_qty' => 0,
+            'taxes' => 0
         ],
         [
             'product_name' => "box of imported chocolates",
             'qty' => 3,
             'type' => "food",
             'imported' => true,
-            'price' => 11.25
+            'price' => 11.25,
+            'price_qty' => 0,
+            'taxes' => 0
         ]
     ]
 ];
 
 foreach ($baskets as &$basket) {
     $taxes = 0;
-    $tot_cost = 0;
+    $totCost = 0;
 
-    foreach ($basket as $kBasket => $vBasket) {
-        foreach ($vBasket as $key => $value) {
+    foreach ($basket as $kBasket => &$vBasket) {
+        $except = in_array($vBasket['type'], $exceptTypes);
+        $imported = $vBasket['imported'];
+        $price = $vBasket['price'];
+        $qty = $vBasket['qty'];
 
-            if ($key == "type" && in_array($value, $exceptTypes)) {
-                $except = true;
-            } else {
-                $except = false;
-            }
-            
-            if ($key == "price"){
-                if(!$except) {
-                    $taxes += $value * BASIC_SALES_TAX / 100;
-                }
-                $tot_cost += $value;
-            }
-
-            if ($key == "imported") {
-                if ($value) {
-                    $taxes += $value * IMPORT_DUTY / 100;;
-                }
-            }
-
-            if ($key == "qty") {
-                $taxes *= $value;
-                $tot_cost *= $value;
-            }
+        if (!$except) {
+            $addBasicSalesTaxValue = $price * BASIC_SALES_TAX / 100;
+            // rounding
+            $addBasicSalesTaxValue = ceil($addBasicSalesTaxValue * 20) / 20;
+            $taxes += $addBasicSalesTaxValue;
         }
+        
+        if ($imported) {
+            $addImportDutyTaxValue = $price * IMPORT_DUTY / 100;
+            // rounding
+            $addImportDutyTaxValue = ceil($addImportDutyTaxValue * 20) / 20;
+            $taxes += $addImportDutyTaxValue;
+        }
+        
+        // totals
+        $priceQty = $price * $qty;
+
+        // print_r($price);
+        // print_r(" * ");
+        // print_r($qty);
+        // print_r(" = ");
+        // print_r($priceQty);
+        
+        // print_r(" --NEXT-- ");
+        
+        $totCost += $priceQty;
+        $totCost += $taxes;
+        
+        // $taxes *= $qty;
+        // $totCost *= $qty;
+
+        // product sales tax
+        print_r($priceQty);
+        print_r("  ");
+        
+        $vBasket['price_qty'] = $priceQty;
+        $vBasket['taxes'] = $taxes;
+
+        $taxes = 0;
+        
     }
+    // print_r(" ---FINE--- ");
+
     $basket['sales_taxes'] = $taxes;
-    $basket['total'] = $tot_cost;
+    $basket['total'] = $totCost;
 }
 
 $return = [
